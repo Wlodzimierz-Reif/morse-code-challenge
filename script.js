9724c739c618d4a239f75cfb778971145c1432ae
Wlodzimierz Reif <Wlodzimierz.Reif@Nology.local>
2020-02-24 20:21:15 +0000

    added randomisation in game

--- script.js
+++ script.js
@@ -77,3 +77,64 @@ const decodeMorse = () => {
     //     return document.querySelector('#text-output').innerHTML = 'Use only morse code characters';
     // }
 }
+
+
+
+
+
+
+const easySets = {
+    "I ate dinner": ".. / .- - . / -.. .. -. -. . .-.",
+    "We had a three-course meal": ".-- . / .... .- -.. / .- / - .... .-. . . / -.-. --- ..- .-. ... . / -- . .- .-..",
+    "Brad came to dinner with us": "-... .-. .- -.. / -.-. .- -- . / - --- / -.. .. -. -. . .-. / .-- .. - .... / ..- ...",
+    "He loves fish tacos": ".... . / .-.. --- ...- . ... / ..-. .. ... .... / - .- -.-. --- ...",
+    "In the end we all felt like we ate too much": ".. -. / - .... . / . -. -.. / .-- . / .- .-.. .-.. / ..-. . .-.. - / .-.. .. -.- . / .-- . / .- - . / - --- --- / -- ..- -.-. ....",
+    "We all agreed; it was a magnificent evening": ".-- . / .- .-.. .-.. / .- --. .-. . . -.. / .. - / .-- .- ... / .- / -- .- --. -. .. ..-. .. -.-. . -. - / . ...- . -. .. -. --."
+}
+
+const mediumSets = {
+    "I hope that when I have built up my savings I will be able to travel to Mexico": ".. / .... --- .--. . / - .... .- - / .-- .... . -. / .. / .... .- ...- . / -... ..- .. .-.. - / ..- .--. / -- -.-- / ... .- ...- .. -. --. ... / .. / .-- .. .-.. .-.. / -... . / .- -... .-.. . / - --- / - .-. .- ...- . .-.. / - --- / -- . -..- .. -.-. ---",
+    "Did you know that along with gorgeous architecture it is home to the largest tamale": "-.. .. -.. / -.-- --- ..- / -.- -. --- .-- / - .... .- - / .- .-.. --- -. --. / .-- .. - .... / --. --- .-. --. . --- ..- ... / .- .-. -.-. .... .. - . -.-. - ..- .-. . / .. - / .. ... / .... --- -- . / - --- / - .... . / .-.. .- .-. --. . ... - / - .- -- .- .-.. .",
+    "Would not it be lovely to enjoy a week soaking up the culture": ".-- --- ..- .-.. -.. / -. --- - / .. - / -... . / .-.. --- ...- . .-.. -.-- / - --- / . -. .--- --- -.-- / .- / .-- . . -.- / ... --- .- -.- .. -. --. / ..- .--. / - .... . / -.-. ..- .-.. - ..- .-. .",
+    "Of all the places to travel Mexico is at the top of my list": "--- ..-. / .- .-.. .-.. / - .... . / .--. .-.. .- -.-. . ... / - --- / - .-. .- ...- . .-.. / -- . -..- .. -.-. --- / .. ... / .- - / - .... . / - --- .--. / --- ..-. / -- -.-- / .-.. .. ... -",
+    "Once you know all the elements it is not difficult to pull together a sentence": "--- -. -.-. . / -.-- --- ..- / -.- -. --- .-- / .- .-.. .-.. / - .... . / . .-.. . -- . -. - ... / .. - / .. ... / -. --- - / -.. .. ..-. ..-. .. -.-. ..- .-.. - / - --- / .--. ..- .-.. .-.. / - --- --. . - .... . .-. / .- / ... . -. - . -. -.-. .",
+    "My mother hemmed and hawed over where to go for dinner": "-- -.-- / -- --- - .... . .-. / .... . -- -- . -.. / .- -. -.. / .... .- .-- . -.. / --- ...- . .-. / .-- .... . .-. . / - --- / --. --- / ..-. --- .-. / -.. .. -. -. . .-."
+}
+
+const hardSets = {
+    "They put her through college and it was her intent to stay with them as long as they needed her": "- .... . -.-- / .--. ..- - / .... . .-. / - .... .-. --- ..- --. .... / -.-. --- .-.. .-.. . --. . / .- -. -.. / .. - / .-- .- ... / .... . .-. / .. -. - . -. - / - --- / ... - .- -.-- / .-- .. - .... / - .... . -- / .- ... / .-.. --- -. --. / .- ... / - .... . -.-- / -. . . -.. . -.. / .... . .-.",
+    "I quickly put on my red winter jacket black snow pants waterproof boots homemade mittens and handknit scarf": ".. / --.- ..- .. -.-. -.- .-.. -.-- / .--. ..- - / --- -. / -- -.-- / .-. . -.. / .-- .. -. - . .-. / .--- .- -.-. -.- . - / -... .-.. .- -.-. -.- / ... -. --- .-- / .--. .- -. - ... / .-- .- - . .-. .--. .-. --- --- ..-. / -... --- --- - ... / .... --- -- . -- .- -.. . / -- .. - - . -. ... / .- -. -.. / .... .- -. -.. -.- -. .. - / ... -.-. .- .-. ..-.",
+    "They sat in a heavy flat bottomed boat each holding a long crooked rod in his hands and eagerly waiting for a bite": "- .... . -.-- / ... .- - / .. -. / .- / .... . .- ...- -.-- / ..-. .-.. .- - / -... --- - - --- -- . -.. / -... --- .- - / . .- -.-. .... / .... --- .-.. -.. .. -. --. / .- / .-.. --- -. --. / -.-. .-. --- --- -.- . -.. / .-. --- -.. / .. -. / .... .. ... / .... .- -. -.. ... / .- -. -.. / . .- --. . .-. .-.. -.-- / .-- .- .. - .. -. --. / ..-. --- .-. / .- / -... .. - .",
+    "Yes there it was all quivering in the warm sunshine its blossom laden branches almost touching the long grass": "-.-- . ... / - .... . .-. . / .. - / .-- .- ... / .- .-.. .-.. / --.- ..- .. ...- . .-. .. -. --. / .. -. / - .... . / .-- .- .-. -- / ... ..- -. ... .... .. -. . / .. - ... / -... .-.. --- ... ... --- -- / .-.. .- -.. . -. / -... .-. .- -. -.-. .... . ... / .- .-.. -- --- ... - / - --- ..- -.-. .... .. -. --. / - .... . / .-.. --- -. --. / --. .-. .- ... ...",
+    "As proud as she was of Jonathan it was Alex who stayed on her mind for a long time after the phone call": ".- ... / .--. .-. --- ..- -.. / .- ... / ... .... . / .-- .- ... / --- ..-. / .--- --- -. .- - .... .- -. / .. - / .-- .- ... / .- .-.. . -..- / .-- .... --- / ... - .- -.-- . -.. / --- -. / .... . .-. / -- .. -. -.. / ..-. --- .-. / .- / .-.. --- -. --. / - .. -- . / .- ..-. - . .-. / - .... . / .--. .... --- -. . / -.-. .- .-.. .-..",
+    "But then maybe Alondra was one of those people who simply took a long time to warm to strangers": "-... ..- - / - .... . -. / -- .- -.-- -... . / .- .-.. --- -. -.. .-. .- / .-- .- ... / --- -. . / --- ..-. / - .... --- ... . / .--. . --- .--. .-.. . / .-- .... --- / ... .. -- .--. .-.. -.-- / - --- --- -.- / .- / .-.. --- -. --. / - .. -- . / - --- / .-- .- .-. -- / - --- / ... - .-. .- -. --. . .-. ..."
+}
+
+const getRandomString = (setOfTestStrings) => {
+    const string = Object.keys(setOfTestStrings);
+    const index = Math.floor(Math.random() * (string.length));
+    const result = string[index];
+    const morseResult = setOfTestStrings[result];
+
+    updateUserInterface(morseResult);
+
+    console.log(result);
+    console.log(morseResult);
+    return result;
+}
+
+const updateUserInterface = (morseString) => {
+    document.getElementById("string-output").innerHTML = morseString;
+}
+
+
+const playEasy = () => {
+    const set = getRandomString(easySets);
+    document.getElementById("string-output").innerHTML = set;
+}
+
+
+
+
+
+
